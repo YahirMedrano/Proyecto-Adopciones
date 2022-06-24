7aa331b21a1f791df6904c3266a86a3d702505fc
AlanWalkup <101841213+AlanWalkup@users.noreply.github.com>
2022-06-23 22:23:17 -0500

    EC001-Verificacion-Pets-Verificacion de modulos
    Modulos que se pueden utilizar
    
    [*] Get (Verificacion con el modulo de ID)
    [*] Get 
    [*] Post
    [*] Put
    [*] Delete

--- routes/pet.js
+++ routes/pet.js
@@ -4,7 +4,6 @@ const petsController = require('../controllers/petsController');
 const petValidator = require("../validations/petValidator");
 const jwtToken = require("../validations/jwtValidation");
 
-router.get('/pet', jwtToken.validateToken, petValidator.id, petsController.getPet);
 router.get('/pets', jwtToken.validateToken, petsController.getPets);
 router.post('/pet', jwtToken.validateToken, petValidator.add, petsController.postPet);
 router.put('/pet', jwtToken.validateToken, petValidator.update, petsController.putPet);
