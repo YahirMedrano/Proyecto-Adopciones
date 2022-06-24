e79f26299adab8bcd9127530f95b5dd9412072e3
AlanWalkup <101841213+AlanWalkup@users.noreply.github.com>
2022-06-23 22:49:47 -0500

    Update pet.js

--- routes/pet.js
+++ routes/pet.js
@@ -4,11 +4,10 @@ const petsController = require('../controllers/petsController');
 const petValidator = require("../validations/petValidator");
 const jwtToken = require("../validations/jwtValidation");
 
-router.get('/pet', jwtToken.validateToken, petValidator.id, petsController.getPet);
+router.get('/pet', jwtToken.validateToken, petsController.getPet);
 router.get('/pets', jwtToken.validateToken, petsController.getPets);
 router.post('/pet', jwtToken.validateToken, petValidator.add, petsController.postPet);
 router.put('/pet', jwtToken.validateToken, petValidator.update, petsController.putPet);
 router.delete('/pet', jwtToken.validateToken, petValidator.id, petsController.deletePet);
 
 module.exports = router;
-
